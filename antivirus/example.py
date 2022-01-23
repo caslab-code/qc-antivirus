04f7c14e79c953782412192b1dce50ffa0aa6937
Chuanqi Xu <466018122@qq.com>
2022-01-23 18:09:39 -0500

    first version of pattern_counter() based on DAGDependency in Qiskit

--- antivirus/example.py
+++ antivirus/example.py
@@ -1,41 +1,45 @@
 from qiskit import QuantumCircuit
-import search_pattern
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import MultipleLocator
 
-qc = QuantumCircuit(3)
+from antivirus import pattern_counter
+
+# define circuit on which we perform pattern matching
+# example circuit is:
+#      ┌───┐          ┌───┐     
+# q_0: ┤ H ├──■────■──┤ H ├─────
+#      └───┘┌─┴─┐┌─┴─┐├───┤
+# q_1: ─────┤ X ├┤ X ├┤ H ├─────
+#           └───┘├───┤├───┤┌───┐
+# q_2: ──■────■──┤ H ├┤ X ├┤ H ├
+#      ┌─┴─┐┌─┴─┐└───┘└─┬─┘└───┘
+# q_3: ┤ X ├┤ X ├───────■───────
+#      └───┘└───┘
+# q_4: ─────────────────────────
+
+qc = QuantumCircuit(5)
 qc.h(0)
 qc.cx(0, 1)
 qc.cx(0, 1)
 qc.h(1)
 qc.h(0)
-qc.cx(0, 1)
-qc.cx(0, 1)
-qc.cx(0, 1)
-qc.h(0)
-qc.cx(0, 1)
-qc.cx(0, 1)
-qc.cx(0, 1)
+qc.cx(2, 3)
+qc.cx(2, 3)
+qc.h(2)
+qc.cx(3, 2)
+qc.h(2)
+# print(qc.draw('text'))
 
-pt = QuantumCircuit(3)
+
+# define pattern to be matched
+# example circuit is:
+#      ┌───┐     
+# q_0: ┤ H ├──■──
+#      └───┘┌─┴─┐
+# q_1: ─────┤ X ├
+#           └───┘
+
+pt = QuantumCircuit(2)
+pt.h(0)
 pt.cx(0, 1)
+# print(pt.draw('text'))
 
-
-qubit_map = [0, 1]
-
-print(search_pattern.search_pattern_defined_bits(qc, pt, qubit_map=qubit_map))
-
-cx_hist = search_pattern.pattern_histogram(qc, 'cx', qubit_map=qubit_map)
-print(cx_hist)
-
-plt.bar(cx_hist.keys(), cx_hist.values())
-plt.xlabel('Number of CX gates')
-plt.gca().xaxis.set_major_locator(MultipleLocator(1))
-plt.ylabel('Count')
-plt.gca().yaxis.set_major_locator(MultipleLocator(1))
-plt.title('Histogram of CX Gates')
-plt.show()
-
-
-# print(search_pattern.pattern_histogram(qc, pt, qubit_map)) # this can also be used
-
+print(pattern_counter(qc, pt))
